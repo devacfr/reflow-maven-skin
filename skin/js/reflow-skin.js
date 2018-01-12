c2e13d6b2961c00c9bddcfeb228569ef7b9b146b
devacfr <christophefriederich@mac.com>
2018-01-12 17:26:06 +0100

    wagon-git commit

--- skin/js/reflow-skin.js
+++ skin/js/reflow-skin.js
@@ -1,10 +1,49 @@
 "use strict";
+/*
+ * By Osvaldas Valutis, www.osvaldas.info Available for use under the MIT
+ * License
+ */
+(function($, window, document, undefined) {
+  $.fn.doubleTapToGo = function(params) {
+    if (!('ontouchstart' in window) && !navigator.msMaxTouchPoints
+        && !navigator.userAgent.toLowerCase().match(/windows phone os 7/i))
+      return false;
+
+    this.each(function() {
+      var curItem = false;
+
+      $(this).on('click', function(e) {
+        var item = $(this);
+        if (item[0] != curItem[0]) {
+          e.stopPropagation();
+          e.preventDefault();
+          curItem = item;
+          timeStamp = e.timeStamp;
+        }
+      });
+
+      $(document).on('click touchstart MSPointerDown', function(e) {
+        var resetItem = true, parents = $(e.target).parents();
+
+        for (var i = 0; i < parents.length; i++)
+          if (parents[i] == curItem[0]) {
+            resetItem = false;
+            break;
+          }
+
+        if (resetItem)
+          curItem = false;
+      });
+    });
+    return this;
+  };
+})(jQuery, window, document);
+
 // Additional skin Javascript
 // ++++++++++++++++++++++++++++++++++++++++++
+!function($) {
 
-!function ($) {
-
-  $(function(){
+  $(function() {
 
     var $window = $(window)
 
@@ -15,21 +54,22 @@
 
     // activate syntax higlighting with highlight.js
     // Note: only run if `hljs` exists
-    if (typeof hljs !== 'undefined')
-    {
+    if (typeof hljs !== 'undefined') {
       // classic encoding with <div class="source"><pre></pre></div>
       // and HTML5 version with <pre><code></code></pre>
-      $('div.source pre, pre code').each(function(i, e) {hljs.highlightBlock(e)});
+      $('div.source pre, pre code').each(function(i, e) {
+        hljs.highlightBlock(e)
+      });
     }
 
     // toc aside bar
     if ($('#toc-sidebar[data-spy=affix]').length) {
-    $('#toc-sidebar').affix({
-      offset: {
-      top: $('#toc-sidebar').offset().top,
-      bottom: ($('footer').outerHeight(true) +
-              $('.subfooter').outerHeight(true)-40) //padding of footer.
-      }
+      $('#toc-sidebar').affix({
+        offset : {
+          top : $('#toc-sidebar').offset().top,
+          bottom : ($('footer').outerHeight(true) + $('.subfooter').outerHeight(true) - 40)
+        // padding of footer.
+        }
       });
     }
 
@@ -39,12 +79,16 @@
       if (navbar.length) {
         size = navbar.height() + 20; // normally 70
       }
-      $('body').css('padding-top',size);
+      $('body').css('padding-top', size);
     }
 
     $(window).resize(resizeTopNavBar);
     // initialize size on start up
     resizeTopNavBar();
+
+    // prevents the browser from opening a URL but allows that if tapped once
+    // again in succession
+    $('.dropdown-submenu').doubleTapToGo();
 
   });
 
