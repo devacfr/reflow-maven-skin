f7604fe9feb4cdca7b81defade863d20f9b0e453
Christophe Friederich <christophefriederich@mac.com>
2020-03-06 16:11:23 +0100

    Publishing Site Docs for Reflow Maven Skin 2.3.0-SNAPSHOT

--- clover/org/devacfr/maven/skins/reflow/context/DocumentContext.js
+++ clover/org/devacfr/maven/skins/reflow/context/DocumentContext.js
@@ -4,7 +4,7 @@ var clover = new Object();
 clover.pageData = {"classes":[{"el":54,"id":994,"methods":[{"el":45,"sc":5,"sl":40},{"el":52,"sc":5,"sl":50}],"name":"DocumentContext","sl":29}]}
 
 // JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
-clover.testTargets = {"test_1":{"methods":[{"sl":40},{"sl":50}],"name":"shouldBuildDocumentContext","pass":true,"statements":[{"sl":41},{"sl":42},{"sl":44},{"sl":51}]}}
+clover.testTargets = {"test_28":{"methods":[{"sl":40},{"sl":50}],"name":"shouldBuildDocumentContext","pass":true,"statements":[{"sl":41},{"sl":42},{"sl":44},{"sl":51}]}}
 
 // JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
-clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [1], [1], [1], [], [1], [], [], [], [], [], [1], [1], [], [], []]
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [28], [28], [28], [], [28], [], [], [], [], [], [28], [28], [], [], []]
