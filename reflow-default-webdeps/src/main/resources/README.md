c2c98e3ce2e2946c03c671a00380cbdf420f7673
Christophe Friederich <christophefriederich@mac.com>
2022-09-27 09:57:22 +0200

    Fix minor errors
    - use $context for velocity variables containing hyphen
    - fix highlight supported language

--- reflow-default-webdeps/src/main/resources/README.md
+++ reflow-default-webdeps/src/main/resources/README.md
@@ -12,7 +12,7 @@ $ npm install
 added 454 packages from 846 contributors and audited 2589 packages in 38.488s
 found 2 vulnerabilities (1 low, 1 high)
   run `npm audit fix` to fix them, or `npm audit` for details
-$ node tools/build.js -t cdn
+$ npm run build-cdn
 ```
 
 package the build result `build/cdn`
--- reflow-maven-skin/src/main/resources/js/reflow-skin.js
+++ reflow-maven-skin/src/main/resources/js/reflow-skin.js
@@ -15,6 +15,10 @@
  */
 "use strict";
 
+let options = {
+  supportedLanguages: ['xml', 'json']
+};
+
 function getViewPort() {
   var e = window, a = 'inner';
   if (!('innerWidth' in window)) {
@@ -90,16 +94,47 @@ var mReflow = function () {
 
   }
 
-  function initHighlight() {
-    // activate syntax higlighting with highlight.js
-    // Note: only run if `hljs` exists
-    if (typeof hljs !== 'undefined') {
+  let highlightJsInitialized = false;
+
+  function refreshHighlight() {
+    if (!highlightJsInitialized) {
+      initHighlight();
+    } else {
       // classic encoding with <div class="source"><pre></pre></div>
       // and HTML5 version with <pre><code></code></pre>
       // asciidoc with <div class="content"><pre></pre></div>
       $('pre.source, div.source pre, pre code, div.content pre').each(function (i, e) {
-        hljs.highlightBlock(e);
+        hljs.highlightElement(e);
       });
+    }
+  }
+
+  function initHighlight() {
+
+    // activate syntax higlighting with highlight.js
+    // Note: only run if `hljs` exists
+    if (typeof hljs !== 'undefined') {
+      hljs.configure({
+        ignoreUnescapedHTML: true,
+        throwUnescapedHTML: false,
+      });
+      const languages_dir = './languages';
+      const supportedLanguages = options.supportedLanguages;
+      let index = 0;
+      if (!highlightJsInitialized) {
+        options.supportedLanguages.forEach(lang => {
+          import(`${languages_dir}/${lang}.min.js`).then(function() {
+            index++;
+            if (index >=supportedLanguages.length) {
+              highlightJsInitialized = true;
+              refreshHighlight();
+            }
+          })
+        });
+      } else {
+        refreshHighlight();
+      }
+
     }
   }
 
@@ -377,6 +412,9 @@ var mReflow = function () {
       initAnchorJs();
       initTooltip();
       refreshScrollSpy();
+    },
+    configure: function(userOptions) {
+      options = inherit(options, userOptions);
     }
   };
 
